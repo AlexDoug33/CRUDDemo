ee3d5e88a015736e0a68d3f19e07f47d569f398e
Alex Douglas <alexdoug33@gmail.com>
2022-03-30 18:08:05 -0300

    CRUDDemo

---
+++ CRUDDemo/Models/PessoaContexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDDemo.Models
+{
+    public class PessoaContexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set;} 
+
+    }
+}
